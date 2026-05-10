47c5012dd885966f4e66be4ddb72c3373f53ff5a
Shahid Shaikh <shahidsocials.007@gmail.com>
2026-05-10 21:55:00 +0530

    Initial commit

---
+++ Backend/inngest/index.js
@@ -0,0 +1,70 @@
+import { Inngest } from "inngest";
+import prisma from "../configs/prisma.js";
+
+// Create Inngest client
+export const inngest = new Inngest({ id: "my-app" });
+
+// Sync user creation
+const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data?.first_name || ""} ${data?.last_name || ""}`,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
+// Sync user deletion
+const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-from-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.delete({
+      where: {
+        id: data.id,
+      },
+    });
+  }
+);
+
+// Sync user updation
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data?.first_name || ""} ${data?.last_name || ""}`,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
