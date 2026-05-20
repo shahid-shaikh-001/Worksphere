06a52c25964cee053222c5a032c883faa46fd2c5
Shahid Shaikh <shahidsocials.007@gmail.com>
2026-05-20 21:45:46 +0530

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -1,41 +1,44 @@
-import express from "express"
-import 'dotenv/config'
-import cors from "cors"
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest,functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
 import workspaceRouter from "./routes/workspaceRoutes.js";
 import { protect } from "./middleware/authMiddleware.js";
 import projectRouter from "./routes/projectRoutes.js";
 import taskRouter from "./routes/taskRoutes.js";
 import commentRouter from "./routes/commentRoutes.js";
 
+const app = express();
 
-const app = express()
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://worksphere-sx6a.vercel.app",
+  "https://worksphere-dusky.vercel.app",
+];
 
-app.use(express.json())
-app.use(cors({
-    origin: [
-    "http://localhost:5173",
-    "https://worksphere-dusky.vercel.app"
-  ],
-  credentials: true,
-}))
-app.use(clerkMiddleware())
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
-app.get('/', (req,res) => res.send('server is Live!'));
+app.options("*", cors());
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => res.send("server is Live!"));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-// Routes
-app.use('/api/workspaces', protect, workspaceRouter)
-app.use('/api/projects', protect, projectRouter)
-app.use('/api/tasks', protect, taskRouter)
-app.use('/api/comments', protect, commentRouter)
+app.use("/api/workspaces", protect, workspaceRouter);
+app.use("/api/projects", protect, projectRouter);
+app.use("/api/tasks", protect, taskRouter);
+app.use("/api/comments", protect, commentRouter);
 
+const PORT = process.env.PORT || 5000;
 
-
-const PORT = process.env.PORT || 5000
-
-
-app.listen(PORT, () => console.log(`Server is Running on Port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server is Running on Port: ${PORT}`));
